8276adc4fa5301db25d7baddeaeceb93b4d3fe4c
GuangHongwei <guanghongwei@xiaomi.com>
2017-10-10 21:52:13 +0800

    Update setpy.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ]
 )
