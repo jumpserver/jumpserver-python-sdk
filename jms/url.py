4ad7208c2086dcfb9fc3a0ad2267793ffa338451
i317280 <zheng.liu02@sap.com>
2017-12-16 17:09:44 +0800

    feat: add asset

--- jms/url.py
+++ jms/url.py
@@ -15,4 +15,5 @@ API_URL_MAPPING = {
     'system-user-auth-info': '/api/assets/v1/system-user/%s/auth-info/',
     'validate-user-asset-permission': '/api/perms/v1/asset-permission/user/validate/',
     'finish-task': '/api/terminal/v1/tasks/%s/',
+    'asset': '/api/assets/v1/assets/%s/',
 }
