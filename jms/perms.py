2a445a7f618f159a0c77ee395ebd50c999c4359a
BaiJiangJie <bugatti_it@163.com>
2018-10-25 19:14:09 +0800

    [Update] 获取用户授权资产，添加分页获取方法

--- jms/perms.py
+++ jms/perms.py
@@ -52,6 +52,28 @@ class PermsMixin:
         else:
             return []
 
+    def get_user_assets_paging(self, user, offset=0, limit=60):
+        """分页获取用户被授权的资产列表
+        [{'hostname': 'x', 'ip': 'x', ...,
+         'system_users_granted': [{'id': 1, 'username': 'x',..}]
+        ]
+        """
+        params = {'offset': offset, 'limit': limit}
+        try:
+            resp = self.http.get('user-assets', pk=user.id, use_auth=True, params=params)
+            resp_json = resp.json()
+            results = resp_json.get('results')
+            total = resp_json.get('count')
+        except (RequestError, ResponseError) as e:
+            logger.error("{}".format(e))
+            return [], 0
+
+        if resp.status_code == 200:
+            assets = Asset.from_multi_json(results)
+            return assets, total
+        else:
+            return [], 0
+
     def get_user_asset_groups(self, user):
         """获取用户授权的资产组列表
         [{'name': 'group1', 'comment': 'x', "assets_granted": ["id": "", "],}, ..]
